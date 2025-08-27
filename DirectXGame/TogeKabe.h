070dd3c83fecdd0356ab57c5451d2460601744c1
shimateruki <k024g0032@g.neec.ac.jp>
2025-08-28 08:04:43 +0900

    終わり

---
+++ DirectXGame/TogeKabe.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "KamataEngine.h"
+#include "math.h"
+using namespace KamataEngine;
+class TogeKabe 
+{
+	// 初期化関数
+	void Initialize(Model* model, Camera* camera, const Vector3& position);
+	// 更新関数
+	void Update();
+	// 描画関数
+	void Draw();
+	Vector3 GetWorldPosition() const;
+
+	AABB GetAABB();
+
+private:
+	// ワールド変換
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// カメラ
+	Camera* camera_ = nullptr;
+
+	Math* math = nullptr; // 数学ユーティリティクラスのインスタンス
+
+	const float kWidth = 2.0f;  // 幅
+	const float kHeight = 20.0f; // 高さ
+};
